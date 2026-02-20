2c776543d5342b54cd95f744297f6cfc2ea4531b
Fabian Hummel <fabianhummel22501@gmail.com>
2026-02-20 11:59:56 +0100

    Added a loading screen to some pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,28 @@ import './style.css';
 import './markdown.css'
 import { Songplayer } from '@components/music/Songplayer';
 import { Footer } from '@components/shared/Footer';
-import { Loading } from '@components/shared/Loading';
+import useLoading, { Loading } from '@components/shared/Loading';
 import { Navbar } from '@components/shared/Navbar';
 import { HashRouter, Route } from '@solidjs/router';
 import { lazy } from 'solid-js';
 import { render } from 'solid-js/web';
 import App from './App';
 
+function withLoadingScreen<T>(importCb: () => Promise<T>) {
+    return async () => {
+        const { startLoading } = useLoading();
+        const complete = startLoading(0.5);
+        const book = await importCb();
+        complete();
+        return book;
+    }
+}
+
 const Home = lazy(() => import("@pages/Home"));
 const Projects = lazy(() => import("@pages/Projects"));
-const Songs = lazy(() => import("@pages/Songs"));
-const Book = lazy(() => import("@pages/Book"));
-const Github = lazy(() => import("@pages/Github"));
+const Songs = lazy(withLoadingScreen(() => import("@pages/Songs")));
+const Book = lazy(withLoadingScreen(() => import("@pages/Book")));
+const Github = lazy(withLoadingScreen(() => import("@pages/Github")));
 
 render(
     () => (
